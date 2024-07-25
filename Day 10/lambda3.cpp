dc4f23948472fea300dd901aebf08d10578478d4
Programs <121106777+Sarthak-Create@users.noreply.github.com>
2024-07-25 14:01:18 +0530

    Delete Day 10 directory

--- Day 10/lambda3.cpp
+++
@@ -1,8 +0,0 @@
-//function without name is a lambda function
-#include<iostream>
-using namespace std;
-int main(){
-	auto add = [](int a, int b){return a+b;};
-	cout<<"Sum : "<<add(3,4)<<endl;
-	return 0;
-}
